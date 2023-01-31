f8a63402a5023f2d470a025cc869178457ce111e
Adran Farias Carnavale <adran.carnavale@gmail.com>
2023-01-30 22:45:25 -0300

    main file

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/crnvl96/githubClient"
+	"github.com/crnvl96/gptClient"
+	"github.com/crnvl96/parser"
+)
+
 
 func main() {
-	fmt.Println("ChatGPT")
+	args := parser.ArgParser(os.Args)
+	openai_api_key := args[0]
+	github_token := args[1]
+	github_pr_id := args[2]
+
+	client, context := githubClient.GenerateClient(github_token)
+
+	commits, _ := githubClient.GetCommits(client, context, github_pr_id)
+
+	for _, commit := range commits {
+		files := commit.Files
+		for _, file := range files {
+			fileName := file.GetFilename()
+			commitSHA := commit.GetSHA()
+
+			content, _ := githubClient.GetFileContent(client, context, github_pr_id, commitSHA, fileName)
+
+			codeReview := gptClient.GenerateCompletion(openai_api_key, content)
+
+			codeReviewText := codeReview.Choices[0].Text
+
+			githubClient.CreateIssue(client, context, fileName, codeReviewText)			
+		}
+	}
+	
 }
